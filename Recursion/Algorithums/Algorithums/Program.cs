74b5d657dea370194bbde3841b44ed20603d19d6
Pankaj Kumar Yadav <payadav@zynga.com>
2022-07-17 05:20:42 +0530

    Ading Algo Project + Tower of Hanoi

---
+++ Recursion/Algorithums/Algorithums/Program.cs
@@ -0,0 +1,12 @@
+﻿using Algorithums.Recursion;
+
+namespace Algorithums
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            TowerOfHanoi.TowerOfHanoiExecute(3,'A','B','C');
+        }
+    }
+}
