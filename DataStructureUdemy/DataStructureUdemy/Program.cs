c5635144a54323de968ffc769a672749b295b66c
Pankaj Kumar Yadav <payadav@MacBook-Pro.local>
2022-08-24 23:45:03 +0530

    Problem Solution Infra

---
+++ DataStructureUdemy/DataStructureUdemy/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+namespace DataStructureUdemy;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var type = typeof(Problem);
+        var types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => type.IsAssignableFrom(p));
+        foreach (var typeVal in types)
+        {
+            var bClass = (Problem)Activator.CreateInstance(typeVal)!;
+            bClass?.Run();
+
+        }
+    }
+}
+
+public class Problem
+{
+    public virtual void Run()
+    {
+        // Console.WriteLine("This is Base Class");
+    }
+}
---
+++ DataStructureUdemy/DataStructureUdemy/Strings/Problem1_SearchInsideString.cs
@@ -0,0 +1,9 @@
+namespace DataStructureUdemy.Strings;
+
+public class Problem1_SearchInsideString : Problem
+{
+    public override void Run()
+    {
+        Console.WriteLine("Class 1");
+    }
+}
